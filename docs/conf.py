2289a5b2b282a919de84ec6624cf2fc1b6419b0d
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2019-02-15 22:45:33 +0100

    sphinx-autodoc-typehints is back once again. It still does not respect custom signatures, but at least it does not crash on decorators anymore! New dependency sphinx-autodoc-typehints.

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosectionlabel',
+    'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
