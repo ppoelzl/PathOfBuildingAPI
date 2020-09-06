d0c8f068be60f02a3240a037677f4c52d7b08723
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2020-09-06 23:34:08 +0200

    Drop support for Python 3.6.

--- setup.py
+++ setup.py
@@ -46,12 +46,11 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     # What does your project relate to?
     keywords="pathofexile poe pathofbuilding pob",
     packages=["pobapi"],
-    install_requires=["dataclasses", "dataslots", "lxml", "requests", "unstdlib"],
+    install_requires=["dataslots", "lxml", "requests", "unstdlib"],
 )
